0ab7070af5d21f4fc736fa6aa0cea0c54600f8ff
joshj <joshjdevl@gmail.com>
2014-07-17 16:33:12 +0200

    ability to scale up and work with fig

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+BROKER_ID=$RANDOM
 sed -r -i "s/(zookeeper.connect)=(.*)/\1=$ZK_PORT_2181_TCP_ADDR/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/(broker.id)=(.*)/\1=$BROKER_ID/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/#(advertised.host.name)=(.*)/\1=$HOST_IP/g" $KAFKA_HOME/config/server.properties
