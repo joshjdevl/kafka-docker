a106e7ca6356365ad37a60e3f66a13e8f511012a
joshj <joshjdevl@gmail.com>
2014-07-18 09:39:57 +0200

    use eth0 for the host address

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+eth0_ipaddr=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 BROKER_ID=$RANDOM
+HOST_IP=$eth0_ipaddr
 sed -r -i "s/(zookeeper.connect)=(.*)/\1=$ZK_PORT_2181_TCP_ADDR/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/(broker.id)=(.*)/\1=$BROKER_ID/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/#(advertised.host.name)=(.*)/\1=$HOST_IP/g" $KAFKA_HOME/config/server.properties
